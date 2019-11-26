7e6a9d0ffbe6a22924bf3d861da7ba11f1875152
Kushal Niroula <niroulakushal31@gmail.com>
2019-11-26 20:55:57 +0545

    Feta : add element helpers

---
+++ element-selector.helper.js
@@ -0,0 +1,8 @@
+const __ = document.querySelector.bind(document);
+const _a = document.querySelectorAll.bind(document);
+/**
+ * Usage:
+   const allLinksInPage = _a("a");
+   const form = __("#form")
+ * 
+ */
