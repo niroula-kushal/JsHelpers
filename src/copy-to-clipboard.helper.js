a9f5cf4ec50961bb64e943330cc0d1b9ec14f6c6
Kushal Niroula <niroulakushal31@gmail.com>
2019-11-27 08:49:20 +0545

    Feat : add Suport for watch method to Clipboard

--- src/copy-to-clipboard.helper.js
+++ src/copy-to-clipboard.helper.js
@@ -2,17 +2,47 @@
 const CLIPBOARD_HELPER = {};
 
 CLIPBOARD_HELPER.COPY_TO_CLIPBOARD_ELMS = _a('.copy-to-clipboard');
+
+CLIPBOARD_HELPER.SELECTORS_TO_WATCH = [];
+
+document.addEventListener('click', e => {
+    const elementToCopyFrom = CLIPBOARD_HELPER.GetElementToCopyFromUsingEvent(e);
+    CLIPBOARD_HELPER.CopyFromElement(elementToCopyFrom);
+});
+
+CLIPBOARD_HELPER.Watch = function(selector) {
+    CLIPBOARD_HELPER.SELECTORS_TO_WATCH.push(selector);
+};
+
 CLIPBOARD_HELPER.COPY_TO_CLIPBOARD_ELMS.forEach(elm => {
     elm.addEventListener('click', e => {
         e.preventDefault();
-        const targetElm = CLIPBOARD_HELPER.getTarget(elm);
-        if (targetElm) {
-            let text = targetElm.value;
-            if (text === undefined) text = targetElm.textContent;
-            CLIPBOARD_HELPER.CopyTextToClipboard(text);
-        }
+        const targetElm = CLIPBOARD_HELPER.GetElementToCopyFrom(elm);
+        CLIPBOARD_HELPER.CopyFromElement(targetElm);
     });
 });
+
+CLIPBOARD_HELPER.GetElementToCopyFromUsingEvent = e => {
+    const targetElm = e.target;
+    for(const selector of CLIPBOARD_HELPER.SELECTORS_TO_WATCH)
+    {
+        if(targetElm.matches(selector)) return targetElm;
+    }
+    for(const selector of CLIPBOARD_HELPER.SELECTORS_TO_WATCH)
+    {
+        const parentMatchingSelectorElement = targetElm.closest(selector);
+        if(parentMatchingSelectorElement) return parentMatchingSelectorElement;
+    }
+    return null;
+};
+
+CLIPBOARD_HELPER.CopyFromElement = elm => {
+    if (elm) {
+        let text = elm.value;
+        if (text === undefined) text = elm.textContent;
+        CLIPBOARD_HELPER.CopyTextToClipboard(text);
+    }
+};
 
 CLIPBOARD_HELPER.CopyTextToClipboard = function(text) {
     if (!navigator.clipboard) {
@@ -42,7 +72,8 @@ CLIPBOARD_HELPER.CopyTextToClipboardUsingTextarea = function(text) {
     document.body.removeChild(textArea);
 }
 
-CLIPBOARD_HELPER.getTarget = function (elm) {
+CLIPBOARD_HELPER.GetElementToCopyFrom = function (elm) {
     let targetStr = elm.dataset.target;
     if (!targetStr) return elm;
+    return __(targetStr);
 };
