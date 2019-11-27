76e9cef4ee98f1d1a3ce0e63b7c563cc9cacc409
Kushal Niroula <niroulakushal31@gmail.com>
2019-11-27 10:19:38 +0545

    Feat : add watch method to image preview helper

--- src/image-preview.helper.js
+++ src/image-preview.helper.js
@@ -1,14 +1,35 @@
-const IMAGE_PREVIEW_HELPER = {};
+const IMAGE_PREVIEW_HELPER = {
+    SELECTORS: []
+};
+
+IMAGE_PREVIEW_HELPER.Watch = selector => IMAGE_PREVIEW_HELPER.SELECTORS.push(selector);
+
+document.body.addEventListener('change', e => {
+    const target = e.target;
+    if(target.hasAttribute("type") && target.getAttribute("type") == "file") {        
+        for(const selector of IMAGE_PREVIEW_HELPER.SELECTORS) {
+            if(target.matches(selector)) {
+                IMAGE_PREVIEW_HELPER.Display(target);
+            }
+        }
+    }
+});
+
 IMAGE_PREVIEW_HELPER.toDisplayElms = _a('.to-display');
 IMAGE_PREVIEW_HELPER.toDisplayElms.forEach(elm => {
     elm.addEventListener('change', function () {
-        const displayInIdSelector = this.dataset.displayin;
-        const displayIn = __(`#${displayInIdSelector}`);
-        if(displayIn) {
-            IMAGE_PREVIEW_HELPER.UpdateDisplayImage(this, displayIn);
-        }
+        IMAGE_PREVIEW_HELPER.Display(elm);
     });
 });
+
+IMAGE_PREVIEW_HELPER.Display = (inputElm) => {
+    const displayInIdSelector = inputElm.dataset.displayIn;
+    const displayIn = __(`#${displayInIdSelector}`);
+    if(displayIn) {
+        IMAGE_PREVIEW_HELPER.UpdateDisplayImage(inputElm, displayIn);
+    }
+};
+
 IMAGE_PREVIEW_HELPER.UpdateDisplayImage = function(input, displayElm) {
     if (input.files && input.files[0]) {
         let reader = new FileReader();
