3be7b84b434176de1e798a44e55abdc7a709c762
Kushal Niroula <niroulakushal31@gmail.com>
2019-11-28 08:52:39 +0545

    Feat : add example for UpdatePageFavIcon

--- src/page.icon.helper.js
+++ src/page.icon.helper.js
@@ -1,9 +1,11 @@
-function updatePageFavicon(iconUrl) {
-    let icon = document.querySelector("link[rel*='icon']");
-    if(icon === null) {
-        icon = document.createElement("link");
-        icon.setAttribute("rel", "icon");
-        document.head.appendChild(icon);
+const PAGE_ICON_HELPER = {
+    UpdatePageFavIcon : function(iconUrl) {
+        let icon = document.querySelector("link[rel*='icon']");
+        if(icon === null) {
+            icon = document.createElement("link");
+            icon.setAttribute("rel", "icon");
+            document.head.appendChild(icon);
+        }
+        icon.href = iconUrl;    
     }
-    icon.href = iconUrl;
-}
+};
