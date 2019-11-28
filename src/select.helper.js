41ec8641f33042fdc4fe33cbfb699a9ae2ed44d0
Kushal Niroula <niroulakushal31@gmail.com>
2019-11-28 10:01:15 +0545

    Feat : add select helper example

--- src/select.helper.js
+++ src/select.helper.js
@@ -1,28 +1,29 @@
-function clearSelectOptions(selectElm) {
-    while(selectElm.firstChild) selectElm.firstChild.remove();
-}
 
-function addDefaultOption(selectElm, defaultOptionText) {
-    var optionText = defaultOptionText || "All";
-    var optionElm = generateOptionElm(optionText, 0);
-    selectElm.appendChild(optionElm);
-}
-
-function addOptions(selectElm, options) {
-    var docFragment = document.createDocumentFragment();
-    for (var option of options) {
-        var optionText = option.text;
-        var optionValue = option.value;
-        var optionSelected = option.selected;
-        var optionElm = generateOptionElm(optionText, optionValue, optionSelected);
-        docFragment.appendChild(optionElm);
+const SELECT_HELPER = {
+    ClearSelectOptions: function (selectElm) {
+        while (selectElm.firstChild) selectElm.firstChild.remove();
+    },
+    AddDefaultOption: function (selectElm, defaultOptionText) {
+        var optionText = defaultOptionText || "All";
+        var optionElm = SELECT_HELPER.GenerateOptionElm(optionText, 0);
+        selectElm.appendChild(optionElm);
+    },
+    AddOptions: function (selectElm, options) {
+        var docFragment = document.createDocumentFragment();
+        for (var option of options) {
+            var optionText = option.text;
+            var optionValue = option.value;
+            var optionSelected = option.selected;
+            var optionElm = SELECT_HELPER.GenerateOptionElm(optionText, optionValue, optionSelected);
+            docFragment.appendChild(optionElm);
+        }
+        selectElm.appendChild(docFragment);
+    },
+    GenerateOptionElm : function(text, value, selected) {
+        return new Option(text, value, false, selected);
     }
-    selectElm.appendChild(docFragment);
-}
+};
 
-function generateOptionElm(text, value, selected) {
-    return new Option(text, value, false, selected);
-}
 /**
  * each option needs to be an object of the following format
  {
